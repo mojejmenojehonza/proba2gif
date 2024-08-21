9b5781d32b1700e53926ac6c37b2f3a57b078db4
mojejmenojehonza <127604251+mojejmenojehonza@users.noreply.github.com>
2024-08-21 12:53:05 +0200

    Update cli.py
    bugfix

--- cli.py
+++ cli.py
@@ -4,12 +4,13 @@ import imageio
 import glob
 import cv2
 import os
+import re
 
 # Constants
-OUTPUT_SIZE = (1024, 1024)
+OUTPUT_IMAGE_SIZE = (1024, 1024)
 TRANSLATION_RADIUS = 3
-FULL_ROTATION = 360
-ROTATION_STEP = 90
+FULL_ROTATION_ANGLE = 360
+ROTATION_ANGLE_STEP = 90
 MIN_RADIUS_RATIO, MAX_RADIUS_RATIO = 4, 2
 BLUR_KERNEL = (9, 9)
 BLUR_STD_DEV = 2
@@ -76,7 +77,7 @@ def crop_image(image, center, size):
 
     return cropped_image
 
-def find_sun_center(image):
+def detect_sun_center(image):
     """Detect the sun's center in the image using the Hough Circle Transform."""
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred_image = cv2.GaussianBlur(gray_image, BLUR_KERNEL, BLUR_STD_DEV)
@@ -115,45 +116,53 @@ def enhance_image(image, brightness, contrast_factor, tint='blue'):
 
     return cv2.normalize(tinted_image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
 
-def process_images(input_dir, output_gif, calibration_image_path, frame_duration, tint, brightness, contrast_factor, loop_count):
+def extract_sort_key(file_path):
+    match = re.search(r"SWAP_(\d+T\d+Z)(?:-(\d+))?", file_path)
+    if match:
+        date_part = match.group(1)
+        number_part = int(match.group(2)) if match.group(2) else -1
+        return (date_part, number_part)
+    return (None, None)
+
+def process_solar_images(input_dir, output_gif, calibration_image_path, frame_duration, tint, brightness, contrast_factor, loop_count):
     """Process images in the input directory and save them as a GIF."""
-    start_path = glob.glob(os.path.join(input_dir, "*-0.png"))[0].split("-0.png")[0]
-    image_paths = [f"{start_path}.png"]
-    image_paths.extend([f"{start_path}-{i}.png" for i in range(len(glob.glob(os.path.join(input_dir, "*.png"))) - 1)])
+    image_paths = glob.glob(os.path.join(input_dir, "*.png"))
+    image_paths = sorted(image_paths, key=extract_sort_key)
 
     gif_frames = []
 
     # Process the calibration image
     calibration_image = cv2.imread(calibration_image_path)
-    calibration_center = find_sun_center(calibration_image)
+    calibration_center = detect_sun_center(calibration_image)
     if calibration_center is None:
         return
 
-    calibration_crop = crop_image(calibration_image, calibration_center, OUTPUT_SIZE)
+    calibration_cropped = crop_image(calibration_image, calibration_center, OUTPUT_IMAGE_SIZE)
 
     for image_path in image_paths:
+        print(f"Processing: {image_path}")
         image = cv2.imread(image_path)
-        sun_center = find_sun_center(image)
+        sun_center = detect_sun_center(image)
         if sun_center is None:
             print(f"No circles detected in {image_path}, skipping.")
             continue
 
         # Find the best translation
         translations = [
-            (tx, ty, calculate_image_difference(calibration_crop, crop_image(image, [tx, ty, sun_center[2]], OUTPUT_SIZE)))
+            (tx, ty, calculate_image_difference(calibration_cropped, crop_image(image, [tx, ty, sun_center[2]], OUTPUT_IMAGE_SIZE)))
             for tx in range(sun_center[0] - TRANSLATION_RADIUS, sun_center[0] + TRANSLATION_RADIUS)
             for ty in range(sun_center[1] - TRANSLATION_RADIUS, sun_center[1] + TRANSLATION_RADIUS)
         ]
         best_tx, best_ty, difference = min(translations, key=lambda x: x[2])
-        cropped_image = crop_image(image, [best_tx, best_ty, sun_center[2]], OUTPUT_SIZE)
+        cropped_image = crop_image(image, [best_tx, best_ty, sun_center[2]], OUTPUT_IMAGE_SIZE)
         if difference > 64000:
             print(f"Image too different ({difference}), possibly corrupted, skipping {image_path}.")
             continue
 
         # Find the best rotation
         rotations = [
-            (angle, calculate_image_difference(calibration_crop, rotate_image(cropped_image, angle)))
-            for angle in range(0, FULL_ROTATION, ROTATION_STEP)
+            (angle, calculate_image_difference(calibration_cropped, rotate_image(cropped_image, angle)))
+            for angle in range(0, FULL_ROTATION_ANGLE, ROTATION_ANGLE_STEP)
         ]
         best_rotation_angle, _ = min(rotations, key=lambda x: x[1])
         rotated_image = rotate_image(cropped_image, best_rotation_angle)
@@ -176,7 +185,7 @@ def main():
     parser.add_argument("--contrast_factor", type=int, default=4, help="Contrast factor (default: 4).")
     parser.add_argument("--loop_count", type=int, default=65535, help="How many times will the output loop (default: 65535/infinite).")
     args = parser.parse_args()
-    process_images(args.input_dir, args.output_file, args.calibration_image, args.frame_duration, args.tint, args.brightness, args.contrast_factor, args.loop_count)
+    process_solar_images(args.input_dir, args.output_file, args.calibration_image, args.frame_duration, args.tint, args.brightness, args.contrast_factor, args.loop_count)
     print(f"Output written to: {args.output_file}")
 
 if __name__ == "__main__":
